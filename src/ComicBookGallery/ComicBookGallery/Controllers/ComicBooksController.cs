081c210cdf7a989604ba5f12a78b64febc43b647
mazi99 <mazino.agodo@gmail.com>
2016-09-08 10:38:29 +0100

    Added Comic Book Detail view

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,12 +11,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the comic books controllaa!");
+            return View();
         }
     }
 }
